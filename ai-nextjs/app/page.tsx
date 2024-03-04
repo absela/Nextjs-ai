1f85a63315f1b067974f03f4d18605a2e5c87757
absela <msatyrap@gmail.com>
2024-03-04 17:32:39 +0100

    setup project

--- ai-nextjs/app/page.tsx
+++ ai-nextjs/app/page.tsx
@@ -1,15 +1,4 @@
-import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>AI Next.js</h1>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={72}
-        height={16}
-      />
-    </div>
-  );
+  return <div>Home</div>
 }
