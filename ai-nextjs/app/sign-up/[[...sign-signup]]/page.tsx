9602a80488237b99afbfde65507db57a588b342f
absela <msatyrap@gmail.com>
2024-03-08 04:54:44 +0100

    change signin and signup to arrow func

--- ai-nextjs/app/sign-up/[[...sign-signup]]/page.tsx
+++ ai-nextjs/app/sign-up/[[...sign-signup]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignUp } from "@clerk/nextjs";
  
-export default function Page() {
+const SignUpPage = () =>{
   return <SignUp afterSignUpUrl="/new-user" redirectUrl="/new-user" />;
 }
+
+export default SignUpPage
--- ai-nextjs/utils/db.ts
+++ ai-nextjs/utils/db.ts
@@ -5,7 +5,8 @@ const globalForPrisma = globalThis as unknown as {
     prisma : PrismaClient | undefined
 }
 
-export const prisma = 
+//if theres a prisma if not then make it,and assign it to the variable of prisma
+export const prisma =
     globalForPrisma.prisma ?? 
         new PrismaClient({
             log :['query'],
